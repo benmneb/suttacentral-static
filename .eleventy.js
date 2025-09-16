ac9890e4ca6b0d9f0a98bd86c1ae334dbad145b4
benmneb <git.irritant899@simplelogin.fr>
2025-09-16 19:49:41 +0800

    feat: dont inline CSS (reverts and improves 2a0f0b3)

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,5 @@
-import CleanCSS from 'clean-css'
-import postCss from 'postcss'
-import autoPrefixer from 'autoprefixer'
-import postCssNesting from 'postcss-nesting'
-
 export default function (eleventyConfig) {
-  eleventyConfig.addFilter('postcss', async function (code) {
-    const result = await postCss([postCssNesting, autoPrefixer]).process(code, {
-      from: undefined,
-    })
-    return new CleanCSS({}).minify(result.css).styles
-  })
+  eleventyConfig.addPassthroughCopy('styles')
 
   return {
     dir: {
