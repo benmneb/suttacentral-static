02bbd07de4a2eba0ebb2655f4fa810579a1d3c25
benmneb <git.irritant899@simplelogin.fr>
2025-09-16 20:08:50 +0800

    feat: minify html

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,20 @@
+import htmlmin from 'html-minifier-terser'
+
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('styles')
+
+  eleventyConfig.addTransform('htmlmin', function (content) {
+    if (!(this.page.outputPath || '').endsWith('.html')) return content
+
+    // https://github.com/terser/html-minifier-terser?tab=readme-ov-file#options-quick-reference
+    let minified = htmlmin.minify(content, {
+      useShortDoctype: true,
+      removeComments: true,
+      collapseWhitespace: true,
+    })
+
+    return minified
+  })
 
   return {
     dir: {
