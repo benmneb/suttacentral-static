352592a39454951cb8dbb05fb66693cf995a62c9
benmneb <git.irritant899@simplelogin.fr>
2026-01-02 22:03:22 +0800

    feat: add cmd/ctrl+k to open search popover

--- scripts/preSearch.js
+++ scripts/preSearch.js
@@ -9,6 +9,8 @@
  * Without JS: Form submits to external search (limited to this domain) with hidden input field
  * With JS: Form submits to /search page with query parameter for client-side search
  *
+ * Also adds `Cmd (Mac) or Ctrl (Windows/Linux) + K` shortcut for opening the search popover
+ *
  * @module preSearch
  */
 ;(async function () {
@@ -23,8 +25,18 @@
 
   const input = document.getElementById('search-input')
   const form = document.getElementById('search-form')
+  const popover = document.getElementById('search-modal')
+  const icon = document.querySelector('button[popovertarget="search-modal"]')
 
   form.action = '/search'
   form.querySelector('input[type="hidden"]')?.remove()
   input.name = 'query'
+  icon.title = 'Cmd/Ctrl+K'
+
+  document.addEventListener('keydown', function (event) {
+    if ((event.metaKey || event.ctrlKey) && event.key === 'k') {
+      event.preventDefault()
+      popover.togglePopover()
+    }
+  })
 })()
