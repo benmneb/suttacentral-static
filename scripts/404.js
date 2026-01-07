92803c2dbc3d943b6992ea311a29792f7dfe393d
benmneb <git.irritant899@simplelogin.fr>
2026-01-07 19:04:42 +0800

    feat: include real 404 pathname in email

--- scripts/404.js
+++ scripts/404.js
@@ -1,9 +1,13 @@
 /**
  * The static 404 page can't know the URL that 404'd without JS.
  * This allows users to check if the 404 also exists on .net
+ * and put the real URL in the email link.
  */
 ;(function () {
   const scRedirectLink = document.getElementById('sc-redirect')
-  if (!scRedirectLink?.href) return
-  scRedirectLink.href = scRedirectLink.href.replace('/404', location.pathname)
+  const mailToLink = document.getElementById('let-me-know')
+  if (scRedirectLink?.href)
+    scRedirectLink.href = scRedirectLink.href.replace('/404', location.pathname)
+  if (mailToLink?.href)
+    mailToLink.href = mailToLink.href.replace('404', location.pathname)
 })()
